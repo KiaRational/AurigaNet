9630a5387b41ecce8bd805f717511dc138b725b9
KiaRational <kiarash.ghasemzadeh@gmail.com>
2023-09-03 16:18:42 +0330

    added loss file in utils to defined losses are for Feature Embedding and Dice loss is for segmentation also added Boundary loss to test it in future . TO DO: add Object Detection Losses ! By SonOfAnton

---
+++ utils/losses.py
@@ -0,0 +1,142 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import os 
+import sys
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(project_root)
+
+from utils.Parameters import Parameters
+
+'''
+  _                         __                      _    _                    
+ | |                       / _|                    | |  (_)                   
+ | |      ___   ___  ___  | |_  _   _  _ __    ___ | |_  _   ___   _ __   ___ 
+ | |     / _ \ / __|/ __| |  _|| | | || '_ \  / __|| __|| | / _ \ | '_ \ / __|
+ | |____| (_) |\__ \\__ \ | |  | |_| || | | || (__ | |_ | || (_) || | | |\__ \
+ |______|\___/ |___/|___/ |_|   \__,_||_| |_| \___| \__||_| \___/ |_| |_||___/
+                                                                              
+'''
+
+class EmbeddingLoss(nn.Module):
+
+    def __init__(self):
+
+        super(CustomLoss, self).__init__()
+
+        self.p = Parameters()
+
+    def forward(self, feature, ground_truth_instance):
+
+        real_batch_size = feature.size(0)
+        
+        F_SIM_i = feature.view(real_batch_size, self.p.feature_size, 1, self.p.grid_y * self.p.grid_x)
+        F_SIM_i = F_SIM_i.expand(real_batch_size, self.p.feature_size, self.p.grid_y * self.p.grid_x, self.p.grid_y * self.p.grid_x).detach()
+        
+        F_SIM_j = feature.view(real_batch_size, self.p.feature_size, self.p.grid_y * self.p.grid_x, 1)
+        F_SIM_j = F_SIM_j.expand(real_batch_size, self.p.feature_size, self.p.grid_y * self.p.grid_x, self.p.grid_y * self.p.grid_x)
+        
+        Norm = (F_SIM_i - F_SIM_j) ** 2
+        Norm = torch.norm(Norm, dim=1).view(real_batch_size, 1, self.p.grid_y * self.p.grid_x, self.p.grid_y * self.p.grid_x)
+
+        # Calculate same instance loss (Loss_Instance_1)
+        Loss_Instance_1 = torch.sum(Norm[ground_truth_instance == 1]) / torch.sum(ground_truth_instance == 1)
+
+        # Calculate different instance loss (Loss_Instance_0)
+
+        Loss_Instance_0 = torch.max([0,self.p.K1 - distancNorme_map[ground_truth_instance == 2]])
+
+        Loss_Instance_0 = torch.sum(Loss_Instance_0) / torch.sum(ground_truth_instance == 2)
+
+        # for one class and no class
+
+        if math.isnan(Loss_Instance_0):
+
+            Loss_Instance_0 = 0
+
+        if math.isnan(Loss_Instance_1):
+
+            Loss_Instance_1 = 0
+
+        Loss = Loss_Instance_1 + Loss_Instance_0
+
+        return Loss
+
+
+
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        Dice_BCE = BCE + dice_loss
+        
+        return Dice_BCE
+
+
+
+
+class BoundaryLoss1(nn.Module):
+    """Boundary Loss proposed in:
+    Alexey Bokhovkin et al., Boundary Loss for Remote Sensing Imagery Semantic Segmentation
+    https://arxiv.org/abs/1905.07852
+    """
+    def __init__(self, theta0=5, theta=11):
+        super().__init__()
+        self.theta0 = theta0
+        self.theta = theta
+
+    def forward(self, pred, gt):
+        """
+        Input:
+            - pred: the output from model (before softmax)
+                    shape (N, C, H, W)
+            - gt: ground truth map
+                    shape (N, H, w)
+        Return:
+            - boundary loss, averaged over mini-batch
+        """  
+        pred = torch.softmax(pred, dim=1)
+        one_hot_gt = self.one_hot(gt, pred.size(1))
+
+        gt_b = self.boundary_map(one_hot_gt, self.theta0)
+        pred_b = self.boundary_map(1 - pred, self.theta0)
+
+        gt_b_ext = self.boundary_map(gt_b, self.theta)
+        pred_b_ext = self.boundary_map(1 - pred_b, self.theta)
+
+        P, R, BF1 = self.compute_metrics(pred_b, gt_b_ext, pred_b_ext, gt_b)
+
+        loss = torch.mean(1 - BF1)
+
+        return loss
+
+    def boundary_map(self, mask, kernel_size):
+        boundary = F.max_pool2d(1 - mask, kernel_size=kernel_size, stride=1, padding=(kernel_size - 1) // 2)
+        boundary -= 1 - mask
+        return boundary
+
+    def compute_metrics(self, pred_b, gt_b_ext, pred_b_ext, gt_b):
+        eps = 1e-7
+        P = (torch.sum(pred_b * gt_b_ext, dim=2) + eps) / (torch.sum(pred_b, dim=2) + eps)
+        R = (torch.sum(pred_b_ext * gt_b, dim=2) + eps) / (torch.sum(gt_b, dim=2) + eps)
+        BF1 = (2 * P * R + eps) / (P + R + eps)
+        return P, R, BF1
+
+    def one_hot(self, tensor, num_classes):
+        one_hot = torch.zeros(tensor.size(0), num_classes, *tensor.size()[1:]).to(tensor.device)
+        one_hot.scatter_(1, tensor.unsqueeze(1), 1)
+        return one_hot
+
